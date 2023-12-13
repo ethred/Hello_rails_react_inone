612094ab9edbdfb06850fd3dcf0d992b1eb2c913
redwan <redtah2007@yahoo.com>
2023-12-13 14:00:03 +0300

    set babelrc and run react test page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,5 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Message.create(text: 'Hello! It\'s great to connect with you')
+Message.create(text: 'Selam New')
+Message.create(text: 'Welcome!')
+Message.create(text: 'Selam Aleykum')
+Message.create(text: 'Bienvenue')
